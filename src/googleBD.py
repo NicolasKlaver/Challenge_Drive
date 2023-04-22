f120d376b8c7867b71c88cd812c9f72a212b8135
Nicolas Klaver <nicolasklaver@hotmail.com>
2023-04-22 19:56:45 -0300

    agrego unique_values

--- src/googleBD.py
+++ src/googleBD.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import json
 import datetime
+import sys
 
 
 class Database:
@@ -71,8 +72,10 @@ class Database:
     
     def insert_data(self, table_name, data):
         try:
+            data_unique= self.get_unique_data(data)
+            
             # Cargar los datos JSON en un objeto Python
-            data_json= json.dumps(data)
+            data_json= json.dumps(data_unique)
             data_dict = json.loads(data_json)
         
             # Crear el cursor
@@ -82,9 +85,9 @@ class Database:
             # Ejecutar la consulta para cada conjunto de datos en la lista
             for item in data_dict:
                 # Construir la consulta SQL
-                sql = """INSERT INTO Inventario 
+                sql = """INSERT INTO {} 
                     (name, extension, owner, visibility, fecha_ultima_modificacion, was_public)
-                    VALUES (%s, %s, %s, %s, %s, %s)"""
+                    VALUES (%s, %s, %s, %s, %s, %s)""".format(table_name)
 
             
                 # Convertir el valor de was_public a un booleano
@@ -128,6 +131,50 @@ class Database:
         
         except mysql.connector.Error as err:
             print(f"Error al actualizar los registros: {err}")
+            
+    def select_all_data(self, table_name):
+        try:
+            self.cursor = self.connection.cursor(dictionary=True)
+            sql = f"SELECT name, extension, owner, visibility, fecha_ultima_modificacion FROM {table_name}"
+            self.cursor.execute(sql)
+            
+            return self.cursor.fetchall()
+        
+        except mysql.connector.Error as err:
+            print(f"Error al seleccionar los datos: {err}")
+    
+    def get_unique_data(self, new_data):
+        unique_data = []
+        existing_data = self.select_all_data("Inventario")
+        
+        if existing_data is None or len(existing_data) == 0:
+            return new_data
+        
+        #print("Base de datos ANTES de convertir:", existing_data)
+        
+        #Tengo que cambiar el campo de datatime a string para poder comparar
+      #  for item in existing_data:
+       #     item['fecha_ultima_modificacion'] = item['fecha_ultima_modificacion'].strftime("%Y-%m-%d %H:%M:%S")
+       # print("Imprimo los nuevos datos a agregar: \n", new_data)
+        
+        #print("Imprimo datos de la base de datos DESPUES DE convertir: \n",existing_data)
+        
+        for new_item in new_data:
+            is_unique = True
+            for existing_item in existing_data:
+                if existing_item['name'] == new_item['name'] and existing_item['extension'] == new_item['extension']:
+                    is_unique = False
+                    break
+            if is_unique:
+                unique_data.append(new_item)
+                
+        if not unique_data:
+            print("No hay datos nuevos que agregar")
+            sys.exit()  # Salir de la función y del programa
+        
+        return unique_data
+
+
         
 
 db = Database(host="localhost", user="root", password="root")
@@ -140,29 +187,27 @@ data= [
   {
     "name": "documento1",
     "extension": "pdf",
-    "owner": "usuario1@gmail.com",
-    "visibility": "privado",
+    "owner": "usuario12@gmail.com",
+    "visibility": "publico",
     "fecha_ultima_modificacion": "2022-04-19 13:00:00"
   },
   {
     "name": "imagen1",
     "extension": "png",
     "owner": "usuario2@gmail.com",
-    "visibility": "publico",
+    "visibility": "privado",
     "fecha_ultima_modificacion": "2022-04-18 10:30:00"
   },
   {
-    "name": "documento2",
+    "name": "documento",
     "extension": "docx",
     "owner": "usuario3@gmail.com",
-    "visibility": "publico",
+    "visibility": "privado",
     "fecha_ultima_modificacion": "2022-04-17 14:45:00"
   }
     ]
 
-#db.insert_data("Inventario", data)
-
-db.update_visibility(table_name="Inventario")
-
+db.insert_data(table_name="Inventario", data = data)
+#db.update_visibility(table_name="Inventario")
 
 db.close_connection()
